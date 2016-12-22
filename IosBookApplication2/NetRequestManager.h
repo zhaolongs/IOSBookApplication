b9e05968852e488d1897d35a44a6fb7404936d97
zhaolong <928343994@qq.com>
2016-12-22 15:22:28 +0800

    readme文件中添加首页网络访问方式

---
+++ IosBookApplication2/NetRequestManager.h
@@ -0,0 +1,13 @@
+//
+//  NetRequestManager.h
+//  IosBookApplication2
+//
+//  Created by  androidlongs on 16/12/22.
+//  Copyright © 2016年  androidlongs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NetRequestManager : NSObject
+
+@end
---
+++ IosBookApplication2/NetRequestManager.m
@@ -0,0 +1,13 @@
+//
+//  NetRequestManager.m
+//  IosBookApplication2
+//
+//  Created by  androidlongs on 16/12/22.
+//  Copyright © 2016年  androidlongs. All rights reserved.
+//
+
+#import "NetRequestManager.h"
+
+@implementation NetRequestManager
+
+@end
